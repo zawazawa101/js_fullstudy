5d8dcf18f0117906ba72aed8eb747989e29ec0c6
xiaojianggege <2804149747@qq.com>
2020-07-23 21:42:35 +0800

    leetcode26. 删除排序数组中的重复项

---
+++ LeetCode/removeDuplicates.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ *  leetcode 26. 删除排序数组中的重复项
+ */
+var removeDuplicates = function(nums) {
+  for(let i = 0; i < nums.length; i++){
+    for(let j = i+1; j < nums.length; j++){
+      if(nums[i] == nums[j]){
+        nums.splice(j,1)
+        j--
+      }
+    }
+  }
+  return nums.length
+};
+console.log(removeDuplicates([0,0,1,1,1,1,1,1,2,2,3,3,4]))
