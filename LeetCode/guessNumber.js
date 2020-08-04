8715585cb6cb480ef9161bbb3081845a7fa49e4d
xiaojianggege <2804149747@qq.com>
2020-08-04 16:35:29 +0800

    预编译

--- LeetCode/guessNumber.js
+++ LeetCode/guessNumber.js
@@ -1,21 +1,18 @@
-// /** 
-//  * Forward declaration of guess API.
-//  * @param {number} num   your guess
-//  * @return 	            -1 if num is lower than the guess number
-//  *			             1 if num is higher than the guess number
-//  *                       otherwise return 0
-//  * var guess = function(num) {}
-//  */
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	            -1 if num is lower than the guess number
+ *			             1 if num is higher than the guess number
+ *                       otherwise return 0
+ * var guess = function(num) {}
+ */
 
-// /**
-//  * @param {number} n
-//  * @return {number}
-//  */
-// var guessNumber = function(n) {
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
     
-// };
-var a = 3
-var changeA = function() {
-    console.log(a)
-}
+};
+
 
