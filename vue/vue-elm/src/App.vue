8d11396bf9fe2c6934cb11c22038aa79b3afa46c
xiaojianggege <2804149747@qq.com>
2020-09-03 17:24:58 +0800

    vue饿了吗项目实战

--- vue/vue-elm/src/App.vue
+++ vue/vue-elm/src/App.vue
@@ -2,7 +2,20 @@
   <div id="app">
     <!-- header 组件 -->
     <v-header :shopInfo="seller"></v-header>
-    <router-view/>
+    <div class="tab">
+      <div class="tab-wrapper">
+        <router-link to="/">商品</router-link>
+      </div>
+      <div class="tab-wrapper">
+        <router-link to="/comment">评论</router-link>
+      </div>
+      <div class="tab-wrapper">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <div class="page">
+      <router-view :data="seller" />
+    </div>  
   </div>
 </template>
 <script>
@@ -36,5 +49,27 @@ export default {
 };
 </script>
 <style lang="stylus" scoped>
+@import './common/stylus/variable.styl';
+.tab
+  width 100%
+  display flex
+  height 36px
+  line-height 36px
+  &-wrapper
+    flex 1
+    text-align center
+    a 
+      display block
+      width 100%
+      color $color-font
+    .router-link-exact-active
+      color $color-red
+      border-bottom 2px solid $color-red
+.page
+  position fixed
+  top 174px
+  left 0
+  right 0
+  bottom 0
 
 </style>
